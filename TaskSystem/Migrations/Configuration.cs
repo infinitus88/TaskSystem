3ce577078c1fd9e65ce59c761c16ab70c108d105
Rustam Minnikhanov <minnikhanovrustam@gmail.com>
2021-02-14 20:10:02 +0300

    Enable migrations.

---
+++ TaskSystem/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TaskSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TaskSystem.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TaskSystem.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
