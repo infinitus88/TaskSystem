84c00dbf1f8a9b104911291164513db50dc04fa5
Rustam Minnikhanov <minnikhanovrustam@gmail.com>
2021-02-14 20:04:34 +0300

    Add project files.

---
+++ TaskSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskSystem.Startup))]
+namespace TaskSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
